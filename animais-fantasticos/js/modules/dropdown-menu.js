3f732ebe007dae3e56cb551a03f64ad0011611f0
Pedro Porsch Spainol <pedroporschspaniol@gmail.com>
2023-06-22 19:05:39 -0300

    DropDown refatorado

--- animais-fantasticos/js/modules/dropdown-menu.js
+++ animais-fantasticos/js/modules/dropdown-menu.js
@@ -1,19 +1,43 @@
 import outsideClick from './outsideclick.js';
 
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default class DropdownMenu {
+  constructor(dropdownMenus, events){
+   this.dropdownMenus = document.querySelectorAll(dropdownMenus);
 
-  function handleClick(event) {
+    this.activeClass = 'active'
+   this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+   
+   if(events === undefined){
+    this.events = ['touchstart', 'click']
+   }else{
+    this.events = events
+   }
+   
+  }
+
+  activeDropdownMenu(event) {
     event.preventDefault();
-    this.classList.add('active');
-    outsideClick(this, ['touchstart', 'click'], () => {
-      this.classList.remove('active');
+    const element = event.currentTarget;
+    element.classList.add(this.activeClass);
+    outsideClick(element, this.events, () => {
+      element.classList.remove(this.activeClass);
     });
   }
 
-  dropdownMenus.forEach((menu) => {
-    ['touchstart', 'click'].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
+  addDropDownMenuEvents(){
+    this.dropdownMenus.forEach((menu) => {
+      this.events.forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdownMenu);
+      });
     });
-  });
+  }
+
+  init(){
+    if(this.dropdownMenus.length){
+      this.addDropDownMenuEvents();
+    }
+    return this
+  }
+
+  
 }
